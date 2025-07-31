d114406cb293927d405a87b49d13f54bb7c40052
Moyaj Bin Alamgir <130137627+MOYAJ-BIN-ALAMGIR@users.noreply.github.com>
2025-07-31 11:41:20 +0600

    Update Users in Identity complete

--- Identity/Controllers/AdminController.cs
+++ Identity/Controllers/AdminController.cs
@@ -11,13 +11,50 @@ namespace Identity.Controllers
         public AdminController(UserManager<AppUser> usrMgr, IPasswordHasher<AppUser> passwordHasher)
             {
             userManager = usrMgr;
-            PasswordHasher = PasswordHasher;
+            PasswordHasher = passwordHasher;
             }
 
-        public AdminController(UserManager<AppUser> usrMgr)
+        public async Task<IActionResult> Update(string id)
         {
-            userManager = usrMgr;
+            AppUser user = await userManager.FindByIdAsync(id);
+            if (user != null)
+                return View(user);
+            else
+                return RedirectToAction("Index");
         }
+        [HttpPost]
+        public async Task <IActionResult> Update (string id, string email, string password)
+        {
+            AppUser user = await userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                if (!string.IsNullOrEmpty(email))
+                    user.Email = email;
+                else
+                    ModelState.AddModelError("", "Email cannot be empty");
+                if (!string.IsNullOrEmpty(password))
+                    user.PasswordHash = PasswordHasher.HashPassword(user, password);
+                else
+                    ModelState.AddModelError("", "Password cannot be empty");
+                if (!string.IsNullOrEmpty(email) && !string.IsNullOrEmpty(password))
+                {
+                    IdentityResult result = await userManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                        return RedirectToAction("Index");
+                    else
+                        Errors(result);
+                }
+            }
+            else
+                ModelState.AddModelError("", "User not found");
+            return View(user);
+        }
+        private void Errors(IdentityResult result)
+        {
+            foreach (IdentityError error in result.Errors)
+                ModelState.AddModelError("", error.Description);
+        }
+
         public IActionResult Index()
         {
             return View(userManager.Users);
