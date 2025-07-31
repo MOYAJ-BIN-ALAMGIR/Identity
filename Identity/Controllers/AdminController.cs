ff514c77a754086014b4abcec7ec3ae49426de96
Moyaj Bin Alamgir <130137627+MOYAJ-BIN-ALAMGIR@users.noreply.github.com>
2025-07-31 11:51:49 +0600

    Delete Users in Identity

--- Identity/Controllers/AdminController.cs
+++ Identity/Controllers/AdminController.cs
@@ -82,5 +82,21 @@ namespace Identity.Controllers
             }
             return View(user);
         }
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            AppUser user = await userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                IdentityResult result = await userManager.DeleteAsync(user);
+                if (result.Succeeded)
+                    return RedirectToAction("Index");
+                else
+                    Errors(result);
+            }
+            else
+                ModelState.AddModelError("", "User Not Found");
+            return View("Index", userManager.Users);
+        }
     }
 }
